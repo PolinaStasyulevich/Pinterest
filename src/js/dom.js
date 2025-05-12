1bcd86eb8c2a47963e195519e9d330c766fa3c28
Polina <polaysta@gmail.com>
2025-05-12 02:07:35 +0200

    dom update

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,6 +1,3 @@
-// работа с DOM: getElement, createEl, селекторы
-
-
 // Получаем корневой контейнер
 const main = document.getElementById('main');
 
@@ -8,6 +5,14 @@ const main = document.getElementById('main');
 // Создаём обёртку
 const container = document.createElement('div');
 container.classList.add('container');
+
+
+
+
+
+
+
+
 
 // ---------- ХЕДЕР ----------
 
@@ -17,30 +22,68 @@ header.classList.add('header');
 // Логотип
 const logo = document.createElement('div');
 logo.classList.add('logo');
-logo.textContent = 'Pinterest';
+
+const iconLogo = document.createElement('i');
+iconLogo.classList.add('fa-brands', 'fa-pinterest', 'logo-icon');
+
+const text = document.createElement('span');
+text.classList.add('logo-text');
+text.textContent = 'Pinterest';
+
+logo.append(iconLogo, text);
+
+
 
 // Поиск
+const searchWrapper = document.createElement('div');
+searchWrapper.classList.add('search-wrapper');
+
+const iconSearch = document.createElement('i');
+iconSearch.classList.add('fa-solid', 'fa-magnifying-glass', 'search-icon');
+
 const search = document.createElement('input');
 search.classList.add('search');
 search.setAttribute('type', 'text');
-search.setAttribute('placeholder', 'поиск...');
+search.setAttribute('placeholder', 'Идеи простых блюд на ужин, модных образов и многое другое');
+
+searchWrapper.append(iconSearch, search);
+
+
 
 // Кнопка "Выбрать доску"
 const boardBtn = document.createElement('button');
 boardBtn.classList.add('board-button');
 boardBtn.textContent = 'Выбрать доску ▼';
 
-// Добавляем в хедер
-header.append(logo, search, boardBtn);
 
-// ---------- КАРТОЧКА ----------
 
+
+// Добавляем всё в хедер
+header.append(logo, searchWrapper, boardBtn);
+
+
+
+
+
+
+// ---------- GALLERY ----------
+
+const gallery = document.createElement('div');
+gallery.classList.add('gallery');
+
+// Карточка
 const card = document.createElement('div');
 card.classList.add('card');
 
 // Изображение
 const image = document.createElement('div');
 image.classList.add('card-image');
+
+// Кнопка "⋯"
+const menuButton = document.createElement('button');
+menuButton.classList.add('card-menu-button');
+menuButton.innerHTML = '⋯';
+image.append(menuButton);
 
 // Аватар и описание
 const meta = document.createElement('div');
@@ -53,12 +96,11 @@ const description = document.createElement('p');
 description.classList.add('description');
 description.textContent = 'Описание';
 
-// Собираем карточку
 meta.append(avatar, description);
 card.append(image, meta);
 
-// Вставляем всё в контейнер
-container.append(header, card);
+// ---------- СБОРКА ----------
 
-// И добавляем в main
+gallery.append(card);
+container.append(header, gallery);
 main.append(container);
