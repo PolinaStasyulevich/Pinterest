867c053a8b84e5152428e28459ee2ee7989b9eb7
statyulevich-polina <stasyulevich-polina@mail.ru>
2025-05-19 17:55:52 +0300

    Add mockapi integration

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,18 +1,9 @@
 // Получаем корневой контейнер
 const main = document.getElementById('main');
 
-
 // Создаём обёртку
 const container = document.createElement('div');
 container.classList.add('container');
-
-
-
-
-
-
-
-
 
 // ---------- ХЕДЕР ----------
 
@@ -32,8 +23,6 @@ text.textContent = 'Pinterest';
 
 logo.append(iconLogo, text);
 
-
-
 // Поиск
 const searchWrapper = document.createElement('div');
 searchWrapper.classList.add('search-wrapper');
@@ -48,58 +37,60 @@ search.setAttribute('placeholder', 'Идеи простых блюд на ужи
 
 searchWrapper.append(iconSearch, search);
 
-
-
 // Кнопка "Выбрать доску"
 const boardBtn = document.createElement('button');
 boardBtn.classList.add('board-button');
 boardBtn.textContent = 'Выбрать доску ▼';
 
-
-
-
 // Добавляем всё в хедер
 header.append(logo, searchWrapper, boardBtn);
-
-
-
-
-
 
 // ---------- GALLERY ----------
 
 const gallery = document.createElement('div');
 gallery.classList.add('gallery');
 
-// Карточка
-const card = document.createElement('div');
-card.classList.add('card');
-
-// Изображение
-const image = document.createElement('div');
-image.classList.add('card-image');
-
-// Кнопка "⋯"
-const menuButton = document.createElement('i');
-menuButton.classList.add('fa-solid', 'fa-ellipsis','card-menu-button');
-image.append(menuButton);
-
-// Аватар и описание
-const meta = document.createElement('div');
-meta.classList.add('card-meta');
-
-const avatar = document.createElement('div');
-avatar.classList.add('avatar');
-
-const description = document.createElement('p');
-description.classList.add('description');
-description.textContent = 'Интерьер';
-
-meta.append(avatar, description);
-card.append(image, meta);
-
-// ---------- СБОРКА ----------
-
-gallery.append(card);
+// Вставка галереи и хедера
 container.append(header, gallery);
 main.append(container);
+
+// ---------- ЗАГРУЗКА ДАННЫХ ----------
+
+fetch('https://6829fe1fab2b5004cb357623.mockapi.io/photos')
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(photo => {
+      // Карточка
+      const card = document.createElement('div');
+      card.classList.add('card');
+
+      // Изображение
+      const image = document.createElement('div');
+      image.classList.add('card-image');
+      image.style.backgroundImage = `url(${photo.image})`;
+
+      // Кнопка "⋯"
+      const menuButton = document.createElement('i');
+      menuButton.classList.add('fa-solid', 'fa-ellipsis', 'card-menu-button');
+      image.append(menuButton);
+
+      // Аватар и описание
+      const meta = document.createElement('div');
+      meta.classList.add('card-meta');
+
+      const avatar = document.createElement('div');
+      avatar.classList.add('avatar');
+      avatar.style.backgroundImage = `url(${photo.avatar})`;
+
+      const description = document.createElement('p');
+      description.classList.add('description');
+      description.textContent = photo.description;
+
+      meta.append(avatar, description);
+      card.append(image, meta);
+      gallery.append(card);
+    });
+  })
+  .catch(err => {
+    console.error('Ошибка загрузки данных:', err);
+  });
